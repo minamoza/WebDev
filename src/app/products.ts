e10877116823b95c62d4a23e7508c684f9db30a5
minamoza <61341637+minamoza@users.noreply.github.com>
2021-02-24 11:31:28 +0600

    Add files via upload

---
+++ src/app/products.ts
@@ -0,0 +1,77 @@
+export const products = [
+  {
+    image: 'https://m.media-amazon.com/images/I/71YZlXXFktL._AC_UL640_FMwebp_QL65_.jpg',
+    name: 'TCL 43S325 43 Inch 1080p Smart LED Roku TV (2019)',
+    description: 'A full high definition display paired with an advanced refresh rate, and dual-band Wi-Fi allows you to capture every moment. With a sleek, modern design it looks great whether it is off or on. No more flipping through inputs or wading through complicated menus. Like apps on a smartphone, all your favorites are at your fingertips from the instant you turn it on. Choose from thousands of streaming channels that feature over 500,000 movies and TV episodes plus live sports, news, music, kids and family, food, science and tech, fitness, foreign language, and so much more.',
+    rating: 4.5,
+    src: 'https://www.amazon.com/TCL-43S325-Inch-1080p-Smart/dp/B07G9W9ZR9/ref=sr_1_4?dchild=1&field-shipping_option-bin=3242350011&pf_rd_i=16225009011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=85a9188d-dbd5-424e-9512-339a1227d37c&pf_rd_r=892JGN1J6CBHW4J9MBEY&pf_rd_s=merchandised-search-5&pf_rd_t=101&qid=1588978526&rnid=1266092011&s=electronics&sr=1-4'
+  },
+  {
+    image: 'https://m.media-amazon.com/images/I/81oVSSITRQL._AC_UY436_FMwebp_QL65_.jpg',
+    name: 'ASUS VivoBook 15 F515 Thin and Light Laptop',
+    description: 'Whether for work or play, ASUS VivoBook F515 is the entry-level laptop that delivers powerful performance and immersive visuals. It is NanoEdge display boasts wide 178 degree viewing angles. Inside, it’s powered by an Intel Core i3 processor with 4GB RAM and 128GB of storage.',
+    rating: 4.3,
+    src: 'https://www.amazon.com/ASUS-i3-1005G1-Processor-Fingerprint-F515JA-AH31/dp/B08H4YTTLP/ref=sr_1_4?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=TECD62H5PSTZ9A9EEPE9&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093964&sprefix=laptops%2Caps%2C236&sr=8-4'
+
+  },
+  {
+    image: 'https://m.media-amazon.com/images/I/71rw+F2aLHL._AC_UY436_FMwebp_QL65_.jpg',
+    name: 'Samsung Galaxy Tab S7+ Wi-Fi, Mystic Black - 128 GB',
+    description: 'PC PERFORMANCE. TABLET PORTABILITY: Transform your tablet into a PC experience with DeX mode and the optional keyboard¹ with expanded trackpad.',
+    rating: 5,
+    src: 'https://www.amazon.com/Samsung-Electronics-Galaxy-Wi-Fi-Mystic/dp/B08FBPLZDV/ref=sr_1_4?dchild=1&keywords=tablets&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=TECD62H5PSTZ9A9EEPE9&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614094064&sr=8-4'
+  },
+  {
+    image: 'https://m.media-amazon.com/images/I/51KfFuDhHEL._AC_UL640_FMwebp_QL65_.jpg',
+    name: 'Blackmagic Design ATEM Mini Pro HDMI Live Stream Switcher ',
+    description: 'HDMI Video Inputs - 4 x HDMI type A, 10-bit HD switchable. 2 channel embedded audio.',
+    rating: 4.75,
+    src: 'https://www.amazon.com/Blackmagic-Design-ATEM-Stream-Switcher/dp/B086R79PBC/ref=sr_1_16?dchild=1&fst=as%3Aoff&pf_rd_i=16225007011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=74069509-93ef-4a3c-8dca-a9e3fa773a64&pf_rd_r=HD1V9RQK4BSGNTC3CHZB&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1487012920&rnid=16225007011&s=computers-intl-ship&sr=1-16'
+  },
+  {
+    image: 'https://m.media-amazon.com/images/I/7171Sc9D12L._AC_UY436_FMwebp_QL65_.jpg',
+    name: 'Blackview Smart Watch for Android Phones and iOS Phones',
+    description: '1.3 inch HD touch screen, GPS Activity tracker (pedometer, calories, distance), heart rate monitor, 14 sport modes, sleep monitoring; ',
+    rating: 4.2,
+    src: 'https://www.amazon.com/Blackview-Running-Waterproof-Smartwatch-Compatible/dp/B08QMJRD4Y/ref=sr_1_7?dchild=1&keywords=fitness+GPS&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=67c6cf47-c067-447f-a799-b2cb009a7a6e&pf_rd_r=TECD62H5PSTZ9A9EEPE9&pf_rd_s=merchandised-search-8&pf_rd_t=101&qid=1614094093&sr=8-7'
+  },
+  {
+    image: 'https://m.media-amazon.com/images/I/51NKhnjhpGL._AC_UL640_FMwebp_QL65_.jpg',
+    name: 'Miracase iPhone SE 2020 Case',
+    description: 'CAN WORK with Qi wireless charging. COMPATIBLE: Apple iPhone 8/ iPhone 7 silicone case compatible with 4.7',
+    rating: 4.5,
+    src: 'https://www.amazon.com/iPhone-Silicone-Miracase-Protection-Shockproof/dp/B074T8NYKW/ref=sr_1_3?dchild=1&keywords=phone+cases&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=67c6cf47-c067-447f-a799-b2cb009a7a6e&pf_rd_r=TECD62H5PSTZ9A9EEPE9&pf_rd_s=merchandised-search-8&pf_rd_t=101&qid=1614094179&sr=8-3'
+  },
+  {
+    image: 'https://m.media-amazon.com/images/I/81b+ticOPJL._AC_UY436_FMwebp_QL65_.jpg',
+    name: 'Ninja FD401 Foodi 8-Quart 9-in-1 Deluxe XL Pressure Cooker',
+    description: 'The Ninja Foodi Deluxe—the deluxe pressure cooker that crisps.',
+    rating: 4.1,
+    src: 'https://www.amazon.com/Ninja-FD401-Fryer-Stainless-Pressure-8-Quart/dp/B07S85TPLG/ref=sr_1_2?dchild=1&keywords=small+appliances&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=05e36794-f584-485d-9bd4-6f7d032453fe&pf_rd_r=TECD62H5PSTZ9A9EEPE9&pf_rd_s=merchandised-search-16&pf_rd_t=101&qid=1614094257&sr=8-2'
+  },
+  {
+    image: 'https://m.media-amazon.com/images/I/719hxr+cjEL._AC_UY436_FMwebp_QL65_.jpg',
+    name: 'Presto 03430 Pizzazz Plus Rotating Oven',
+    description: 'Rotating tray turns continuously to assure even baking.',
+    rating: 4,
+    src: 'https://www.amazon.com/Presto-03430-Pizzazz-Plus-Rotating/dp/B00005IBXJ/ref=sr_1_5?dchild=1&keywords=small+appliances&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=05e36794-f584-485d-9bd4-6f7d032453fe&pf_rd_r=TECD62H5PSTZ9A9EEPE9&pf_rd_s=merchandised-search-16&pf_rd_t=101&qid=1614094316&sr=8-5'
+  },
+  {
+    image: 'https://m.media-amazon.com/images/I/61VMRbZO+TL._AC_UL640_FMwebp_QL65_.jpg',
+    name: 'LED Outdoor String Lights 48FT with 2W Dimmable Edison Vintage Shatterproof Bulbs and Commercial Grade Weatherproof',
+    description: 'Baxstel 48ft outdoor string lights for patio is crafted with heavy-duty weathertight technology + SHATTERPROOF PLASTIC 16 hanging LED bulbs. These lights are waterproof and the rubberized, flexible cord is thicker than a traditional rope. Can confidently display these string lights all the year round best choice for outdoor patio lights.',
+    rating: 4.8,
+    src: 'https://www.amazon.com/Shatterproof-Commercial-Weatherproof%EF%BC%8CETL-Heavy-Duty-Decorative/dp/B08R2DQH1D/ref=sr_1_6?dchild=1&keywords=string+lights&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=d108f598-c36c-4aa2-b2be-60da54291f9f&pf_rd_r=TECD62H5PSTZ9A9EEPE9&pf_rd_s=merchandised-search-18&pf_rd_t=101&qid=1614094397&sr=8-6'
+  },
+  {
+    image: 'https://m.media-amazon.com/images/I/71ACxgnGoaL._AC_UL640_FMwebp_QL65_.jpg',
+    name: 'SKIP BO Card Game',
+    description: 'Players use skill and strategy to create sequencing stacks of cards in ascending order (2,3,4…).',
+    rating: 4.5,
+    src: 'https://www.amazon.com/Mattel-Games-1050-SKIP-Card/dp/B0000205XI/ref=sr_1_4?dchild=1&keywords=card+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=FX14AE7ZZE9441HQQC29&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614109834&sr=8-4'
+  }
+
+];
+
+
+
